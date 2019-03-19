277282967b490a5525c4e4cf93ca220de64d3056
Joe <joewandy@gmail.com>
2019-03-19 16:45:47 +0000

    Updated windows release script.

--- release_windows.sh
+++ release_windows.sh
@@ -25,14 +25,23 @@
 # Once the above is done, you can run the following script:
 # "C:\Program Files\Git\git-bash.exe" release_windows.sh
 
-echo "Compiling miner for distribution"
+# https://gist.github.com/DarrenN/8c6a5b969481725a4413
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+PACKAGE_VERSION_NO_WHITESPACE="$(echo -e "${PACKAGE_VERSION}" | tr -d '[:space:]')"
+OUTFILE="sushipool-gpu-miner-windows-${PACKAGE_VERSION_NO_WHITESPACE}.zip"
+echo "Building ${OUTFILE}"
+
 rm -rf node_modules
 yarn
 npm link @nimiq/core
 rm -rf dist
 mkdir dist
 pkg -t node10-windows index.js
-mv index.exe dist/sushipool-gpu-miner.exe
+mv index.exe dist/sushipool-opencl-miner.exe
 
 cp build/Release/nimiq_miner.node dist/
 cp node_modules/leveldown/build/Release/leveldown.node dist/
@@ -45,6 +54,7 @@ cp start_gpu.bat dist
 
 echo "Create zipped"
 cd dist/
-zip -r sushipool-gpu-miner-1.0.5.zip .
+zip -r ${OUTFILE} .
 cd ..
+
 read -p "Press [Enter] key to quit"
--- start_gpu.bat
+++ start_gpu.bat
@@ -1,7 +1,7 @@
 @echo off
 echo "------------------------START Miner----------------------"
 SET UV_THREADPOOL_SIZE=12 
-sushipool-gpu-miner.exe
+sushipool-opencl-miner.exe
 echo "------------------------END Miner----------------------"
 echo "Something went wrong or you exited"
 pause
